20042c2e1714f32006394258f8d282f027e949da
Michele Pesante <pesante.michele.96@gmail.com>
2018-01-22 17:35:52 +0100

    Creazione prototipo

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	/// <summary>
+	/// Variabile per modificare a piacere la transform della camera.
+	/// </summary>
+	private Transform cameraTransform;
+
+	void Awake () {
+		cameraTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.E)) {
+			cameraTransform = GameObject.Find ("CameraPosition_2").transform;
+			transform.position = cameraTransform.position;
+			transform.rotation = cameraTransform.rotation;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			cameraTransform = GameObject.Find ("CameraPosition_1").transform;
+			transform.position = cameraTransform.position;
+			transform.rotation = cameraTransform.rotation;
+		}
+	}
+}
---
+++ Assets/Scripts/PawnScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnScript : MonoBehaviour {
+
+	/// <summary>
+	/// Forza della singola pedina.
+	/// </summary>
+	private int strength;
+
+	/// <summary>
+	/// Tasto per andare in alto.
+	/// </summary>
+	public KeyCode up;
+
+	/// <summary>
+	/// Tasto per andare in basso.
+	/// </summary>
+	public KeyCode down;
+
+	/// <summary>
+	/// Tasto per andare a destra.
+	/// </summary>
+	public KeyCode right;
+
+	/// <summary>
+	/// Tasto per andare in sinistra.
+	/// </summary>
+	public KeyCode left;
+
+	/// <summary>
+	/// Riferimento al game manager ha i riferimenti a tutti gli script.
+	/// </summary>
+	private GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+		if (this.gameObject.name == "Blue_Pawn")
+			strength = 1;
+		if (this.gameObject.name == "Red_Pawn")
+			strength = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (up)) {
+			transform.position = transform.position + Vector3.forward;
+		}
+		if (Input.GetKeyDown (down)) {
+			transform.position = transform.position + Vector3.back;
+		}
+		if (Input.GetKeyDown (right)) {
+			transform.position = transform.position + Vector3.right;
+		}
+		if (Input.GetKeyDown (left)) {
+			transform.position = transform.position + Vector3.left;
+		}
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			if (collision.gameObject.GetComponent<PawnScript> ().strength < this.strength) {
+				Destroy (collision.gameObject);
+			} 
+			else {
+				Destroy (this.gameObject);
+			}
+		}
+	}
+}
