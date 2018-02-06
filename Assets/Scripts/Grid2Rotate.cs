8a4f2e03559c772d8a3a59c2927fd4becc9b27e4
Leandro Dal Canto <leandrodalcanto@gmail.com>
2018-02-06 11:21:15 +0100

    aggiunti tasselli centrali più scuri e rotazione griglie ("q" & "e" per la griglia 1, "u" & "o" per la griglia 2)

---
+++ Assets/Scripts/Grid2Rotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid2Rotate : MonoBehaviour {
+
+    IEnumerator RotateMe(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            StartCoroutine(RotateMe(Vector3.up * 90, 0.8f));
+        }
+        if (Input.GetKeyDown("u"))
+        {
+            StartCoroutine(RotateMe(Vector3.up * -90, 0.8f));
+        }
+    }
+}
