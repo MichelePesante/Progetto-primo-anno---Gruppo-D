348766d734e9f3dc764b8140222c0e6ee24a2d40
DESKTOP-1AII472\Utente <matteo.bogoni97@gmail.com>
2018-03-07 18:38:32 +0100

    Implementazione "guscio" della State machine per la fase di battaglia

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class StateMachine {
+public class StateMachine : MonoBehaviour {
 
 	public static PlayerTurn CurrentPlayerTurn;
 
 	public static MacroPhase CurrentMacroPhase;
 
-	public enum MacroPhase {
+    private BattlePhase _currentPhase;
+
+
+
+    public enum MacroPhase {
 		Start,
 		Setup,
 		Core,
@@ -19,4 +23,127 @@ public static class StateMachine {
 		TurnPlayer1,
 		TurnPlayer2
 	}
+
+
+    //enumeratore per le fasi di battaglia/core
+
+    public enum BattlePhase{
+
+        Rotation,
+        Battle,
+        Reinforce,
+
+    }
+
+
+    public BattlePhase CurrentPhase
+    {
+        get
+        {
+            return _currentPhase;
+        }
+        set
+        {
+            if (CheckPhaseChange(value) == true)
+            {
+                OnPhaseEnd(_currentPhase);
+                _currentPhase = value;
+                OnPhaseStart(_currentPhase);
+            }
+            else
+            {
+                Debug.Log("Non è possibile passare da " + _currentPhase + " a " + value);
+            }
+        }
+    }
+
+
+    
+    bool CheckPhaseChange (BattlePhase newPhase)
+    {
+        switch (newPhase)   
+        {
+            case BattlePhase.Rotation:
+            case BattlePhase.Battle:
+                if (CurrentPhase != BattlePhase.Rotation)
+                    return false;
+                return true;
+                break;
+            case BattlePhase.Reinforce:
+                if (CurrentPhase != BattlePhase.Battle)
+                    return false;
+                return true;
+                break;
+            default:
+                    return false;
+                return true;
+                break;
+        }
+    }
+
+
+    #region State Machine
+
+    void OnPhaseStart(BattlePhase newPhase) {
+
+        switch (newPhase)
+        {
+            case BattlePhase.Rotation:
+                Debug.Log("Sono entrato nello stato di " + newPhase);
+                break;
+            case BattlePhase.Battle:
+                Debug.Log("Sono entrato nello stato di " + newPhase);
+                break;
+            case BattlePhase.Reinforce:
+                Debug.Log("Sono entrato nello stato di " + newPhase);
+                break;
+            default:
+                Debug.Log("Sono entrato in uno stato sconosciuto " + newPhase);
+                break;
+        }
+
+    }
+
+    void OnPhaseUpdate() {
+
+        switch (CurrentPhase)
+        {
+            case BattlePhase.Rotation:
+                Debug.Log("Sono nello stato di " + CurrentPhase);
+                break;
+            case BattlePhase.Battle:
+                Debug.Log("Sono nello stato di " + CurrentPhase);
+                break;
+            case BattlePhase.Reinforce:
+                Debug.Log("Sono nello stato di " + CurrentPhase);
+                break;
+            default:
+                Debug.Log("Sono in uno stato sconosciuto " + CurrentPhase);
+                break;
+        }
+
+
+    }
+
+    void OnPhaseEnd(BattlePhase oldPhase) {
+
+        switch (oldPhase)
+        {
+            case BattlePhase.Rotation:
+                Debug.Log("Sto uscendo dallo stato di " + oldPhase);
+                break;
+            case BattlePhase.Battle:
+                Debug.Log("Sto uscendo dallo stato di " + oldPhase);
+                break;
+            case BattlePhase.Reinforce:
+                Debug.Log("Sto uscendo dallo stato di " + oldPhase);
+                break;
+            default:
+                Debug.Log("Sto uscendo da uno stato sconosciuto " + oldPhase);
+                break;
+        }
+
+    }
+
+    #endregion
 }
