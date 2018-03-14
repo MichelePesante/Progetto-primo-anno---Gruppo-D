1489257fecb8232ef372b3d0090cfb41fdf402e9
Michele Pesante <pesante.michele.96@gmail.com>
2018-03-14 21:34:51 +0100

    Aggiunti robots alla scena con relativi scriptable objects

---
+++ Assets/Scripts/NewScripts/RobotController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+
+	public RobotData Data;
+
+	private int strength;
+	private int upgrade;
+	private int abilità_1;
+	private int abilità_2;
+	private int abilità_3;
+	private int abilità_4;
+	private RobotData InstanceData;
+
+	// Use this for initialization
+	void Start () {
+		Setup ();
+	}
+
+	private void Setup () {
+		if (!Data) {
+			return;
+		}
+		InstanceData = Instantiate <RobotData> (Data);
+		strength = Data.Strength;
+		upgrade = Data.Upgrade;
+		abilità_1 = Data.Abilità_1;
+		abilità_2 = Data.Abilità_2;
+		abilità_3 = Data.Abilità_3;
+		abilità_4 = Data.Abilità_4;
+	}
+}
