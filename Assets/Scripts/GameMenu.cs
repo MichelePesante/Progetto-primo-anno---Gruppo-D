3303d87c15ebd76de944ce56a4a6cac1206cb95d
DESKTOP-1AII472\Utente <matteo.bogoni97@gmail.com>
2018-03-14 18:15:06 +0100

    Modifiche UI: aggiunto Menu principale di gioco, aggiunto Menu di pausa in gioco

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameMenu : MonoBehaviour {
+
+
+    public static bool GameIsPaused = false;
+
+    private void Start()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+
+    public GameObject PauseMenu;
+    // Update is called once per frame
+    void Update()
+    {
+        OpenMenu();
+    }
+
+    public void OpenMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
