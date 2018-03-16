0d29fe75851cd2bba3947e0de37bec318f331421
Michele Pesante <pesante.michele.96@gmail.com>
2018-03-16 19:03:45 +0100

    Aggiunta selezione robot giocabile

--- Assets/Scripts/NewScripts/RobotManager.cs
+++ Assets/Scripts/NewScripts/RobotManager.cs
@@ -4,32 +4,45 @@ using UnityEngine;
 
 public class RobotManager : MonoBehaviour {
 
+	public int RobotPlayed;
+	public int MaxRobotToPlay = 2;
+
+	[Header ("Liste Robot")]
 	public List<RobotController> RobotCurvi;
 	public List<RobotController> RobotQuadrati;
 	public List<RobotController> RobotCurviGiocati;
 	public List<RobotController> RobotQuadratiGiocati;
-	public int RobotPlayed;
-	public int MaxRobotToPlay = 2;
 
+	[Header ("Posizioni Robot")]
+	public GameObject[] PosizioniRobotCurvi;
+	public GameObject[] PosizioniRobotQuadrati;
 
 	private int currentFirstRobotCurvo;
 	private int currentFirstRobotQuadrato;
 	private int robotToPlay;
 	private int maxRobotInHand = 4;
+	private Vector3[] standardPositionsCurvi = new Vector3[4];
+	private Vector3[] standardPositionsQuadrati = new Vector3[4];
+	private Vector3[] highlightedPositionsCurvi = new Vector3[4];
+	private Vector3[] highlightedPositionsQuadrati = new Vector3[4];
 
 	// Use this for initialization
 	void Start () {
-		
+		SetPositions (PosizioniRobotCurvi);
+		SetPositions (PosizioniRobotQuadrati);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		SwitchRobotToPlay ();
 		if (FindObjectOfType<TurnManager> ().CurrentPlayerTurn == TurnManager.PlayerTurn.P1_Turn) {
+			SwitchRobotToPlay (PosizioniRobotCurvi, standardPositionsCurvi, highlightedPositionsCurvi);
 			PlayRobot (RobotCurvi, RobotCurviGiocati);
+			RobotPositioning (RobotCurvi, PosizioniRobotCurvi);
 		}
 		if (FindObjectOfType<TurnManager> ().CurrentPlayerTurn == TurnManager.PlayerTurn.P2_Turn) {
+			SwitchRobotToPlay (PosizioniRobotQuadrati, standardPositionsQuadrati, highlightedPositionsQuadrati);
 			PlayRobot (RobotQuadrati, RobotQuadratiGiocati);
+			RobotPositioning (RobotQuadrati, PosizioniRobotQuadrati);
 		}
 		SwitchPlacingTurn ();
 	}
@@ -77,6 +90,7 @@ public class RobotManager : MonoBehaviour {
 					RemoveRobotFromList (_listToPlayRobotFrom);
 					RobotPlayed++;
 					_hit.collider.gameObject.GetComponentInChildren<ColliderController> ().IsPlaceable = false;
+					robotToPlay = 0;
 				}
 			}
 		}
@@ -92,6 +106,7 @@ public class RobotManager : MonoBehaviour {
 					RemoveRobotFromList (_listToPlayRobotFrom);
 					RobotPlayed++;
 					_hit.collider.gameObject.GetComponentInChildren<ColliderController> ().IsPlaceable = false;
+					robotToPlay = 0;
 				}
 			}
 		}
@@ -100,16 +115,51 @@ public class RobotManager : MonoBehaviour {
 	/// <summary>
 	/// Funzione che permette di cambiare il focus da un robot ad un altro tramite rotellina.
 	/// </summary>
-	public void SwitchRobotToPlay () {
+	public void SwitchRobotToPlay (GameObject[] _positionToHighlight, Vector3[] _standardPositions, Vector3[] _highlightedPositions) {
 		if (Input.GetAxis ("Mouse ScrollWheel") > 0 && robotToPlay < maxRobotInHand - 1) {
 			robotToPlay++;
 		}
 		if (Input.GetAxis ("Mouse ScrollWheel") < 0 && robotToPlay > 0) {
 			robotToPlay--;
 		}
+
+		switch (robotToPlay) {
+		case 0:
+			_positionToHighlight [0].transform.position = _highlightedPositions [0];
+			_positionToHighlight [1].transform.position = _standardPositions [1];
+			_positionToHighlight [2].transform.position = _standardPositions [2];
+			_positionToHighlight [3].transform.position = _standardPositions [3];
+			break;
+		case 1:
+			_positionToHighlight [0].transform.position = _standardPositions [0];
+			_positionToHighlight [1].transform.position = _highlightedPositions [1];
+			_positionToHighlight [2].transform.position = _standardPositions [2];
+			_positionToHighlight [3].transform.position = _standardPositions [3];
+			break;
+		case 2:
+			_positionToHighlight [0].transform.position = _standardPositions [0];
+			_positionToHighlight [1].transform.position = _standardPositions [1];
+			_positionToHighlight [2].transform.position = _highlightedPositions [2];
+			_positionToHighlight [3].transform.position = _standardPositions [3];
+			break;										
+		case 3:										   
+			_positionToHighlight [0].transform.position = _standardPositions [0];
+			_positionToHighlight [1].transform.position = _standardPositions [1];
+			_positionToHighlight [2].transform.position = _standardPositions [2];
+			_positionToHighlight [3].transform.position = _highlightedPositions [3];
+			break;
+		default:
+			break;
+		}
 	}
 
 	#endregion
+
+	private void RobotPositioning (List<RobotController> _listToPositionRobotFrom, GameObject[] _robotPositions) {
+		for (int i = 0; i < maxRobotInHand; i++) {
+			_listToPositionRobotFrom [i].transform.position = _robotPositions[i].transform.position;
+		}
+	}
 
 	private void RemoveRobotFromList (List<RobotController> _listToRemoveRobotFrom) {
 		_listToRemoveRobotFrom.Remove (_listToRemoveRobotFrom [robotToPlay]);
@@ -119,6 +169,22 @@ public class RobotManager : MonoBehaviour {
 		if (RobotPlayed == MaxRobotToPlay) {
 			FindObjectOfType<TurnManager> ().ChangeTurn ();
 			RobotPlayed = 0;
+		}
+	}
+
+	private void SetPositions (GameObject[] _positionToSet) {
+		if (_positionToSet == PosizioniRobotCurvi) {
+			for (int i = 0; i < standardPositionsCurvi.Length; i++) {
+				standardPositionsCurvi [i] = _positionToSet [i].transform.position;
+				highlightedPositionsCurvi [i] = standardPositionsCurvi [i] + Vector3.up;
+			}
+		}
+
+		if (_positionToSet == PosizioniRobotQuadrati) {
+			for (int i = 0; i < standardPositionsQuadrati.Length; i++) {
+				standardPositionsQuadrati [i] = _positionToSet [i].transform.position;
+				highlightedPositionsQuadrati [i] = standardPositionsQuadrati [i] + Vector3.up;
+			}
 		}
 	}
 }
