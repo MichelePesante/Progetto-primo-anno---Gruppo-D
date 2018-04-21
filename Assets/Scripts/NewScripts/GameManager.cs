6b1a644354a12d4709051e7a292eba8553931f08
Michele Pesante <pesante.michele.96@gmail.com>
2018-04-21 04:28:51 +0200

    Aggiornato scena e implementati nuovi modelli 3D

---
+++ Assets/Scripts/NewScripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (TurnManager.Instance.ScoreP1 >= 5) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+
+		if (TurnManager.Instance.ScoreP2 >= 5) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+	}
+}
