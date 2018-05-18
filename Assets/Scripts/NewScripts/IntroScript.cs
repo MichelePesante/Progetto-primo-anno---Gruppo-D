c93a791ef971eb79bbe421ed7d13dc1a6b2fb6d6
Michele Pesante <pesante.michele.96@gmail.com>
2018-05-18 23:35:22 +0200

    Aggiunto filmato di introduzione

--- Assets/Scripts/NewScripts/IntroScript.cs
+++ Assets/Scripts/NewScripts/IntroScript.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class IntroScript : MonoBehaviour {
-	
+
+	private VideoPlayer vid;
+
+	void Start () {
+		vid = FindObjectOfType<VideoPlayer> ();
+		vid.Play ();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown (KeyCode.Space))  {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+		if (vid.isPlaying == false) {
 			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 		}
 	}
