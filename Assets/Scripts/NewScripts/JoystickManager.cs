2eb001397c977054b2ef8d58950ef81dac4966d7
Michele Pesante <pesante.michele.96@gmail.com>
2018-05-24 12:57:24 +0200

    Test Joystick

---
+++ Assets/Scripts/NewScripts/JoystickManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
