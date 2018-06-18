fe986bfcfea6736afb920875c55238906ed96625
Utente-PC\Utente <mr.dna91@gmail.com>
2018-06-18 18:05:31 +0200

    Ampliato AudioManager

--- Assets/Scripts/NewScripts/AudioManager.cs
+++ Assets/Scripts/NewScripts/AudioManager.cs
@@ -8,4 +8,21 @@ public class AudioManager : MonoBehaviour {
     public AudioClip MenuSelect;
     public AudioClip MenuStartGame;
     public AudioClip GameGridRotation;
+    public AudioClip GameQuadCardMovement;
+    public AudioClip GameCurveCardMovement;
+    public AudioClip GameConfirm;
+    //public AudioClip GameCurve1Attack;
+    //public AudioClip GameCurve2Attack;
+    //public AudioClip GameCurve3Attack;
+    //public AudioClip GameCurve4Attack;
+    //public AudioClip GameQuad1Attack;
+    //public AudioClip GameQuad2Attack;
+    //public AudioClip GameQuad3Attack;
+    //public AudioClip GameQuad4Attack;
+    public AudioClip GameEnergy180;
+    public AudioClip GameEnergyDoubleUpgrade;
+    public AudioClip GameEnergyCollision;
+    public AudioClip GameWinQuad;
+    public AudioClip GameWinCurve;
+
 }
